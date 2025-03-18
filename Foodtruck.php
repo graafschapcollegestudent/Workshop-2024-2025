074470b49c95c76e8a4f59b9a93de7ce33142262
hadi <hadi.idle@student.graafschapcollege.nl>
2025-03-18 10:28:25 +0100

    Hallo

---
+++ Foodtruck.php
@@ -0,0 +1,8 @@
+<?php
+
+$dishes = [
+    ['name' => 'Loempia', 'info' => 'Heerlijke loempia met verse groenten.', 'allergens' => 'Gluten, Soja', 'price' => '3.50', 'image' => 'img/LoempiaFoto.jpg'],
+    ['name' => 'Nasi Goreng', 'info' => 'Gebakken rijst met groenten en kip.', 'allergens' => 'Gluten, Ei', 'price' => '7.00', 'image' => 'img/NasiGoreng.jpg'],
+    ['name' => 'Saté Ajam', 'info' => 'Kipsaté met pindasaus.', 'allergens' => 'Pinda, Soja', 'price' => '6.00', 'image' => 'img/SateAjam.jpg']
+];
+?>
